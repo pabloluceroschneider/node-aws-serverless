b065c548b5eda5329a7f6e0e2b12e32e6368854b
pabloluceroschneider <pabloluceroschneider@gmail.com>
2023-07-06 22:41:03 -0300

    Updates

--- 2-HelloWorld/handler.js
+++ 2-HelloWorld/handler.js
@@ -5,7 +5,7 @@ module.exports.ping = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Hello World from aws',
         input: event,
       },
       null,
